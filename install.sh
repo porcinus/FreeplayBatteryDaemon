5aaf27f6d6598af3ae004e8178a1a594e86ce5f8
porcinus <37425709+porcinus@users.noreply.github.com>
2019-07-07 01:49:39 +0200

    Update install.sh
    add compile

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+g++ nns-freeplay-battery-daemon.cpp -o nns-freeplay-battery-daemon
 cp nns-freeplay-battery-daemon.service /lib/systemd/system/nns-freeplay-battery-daemon.service
 systemctl enable nns-freeplay-battery-daemon.service
 systemctl start nns-freeplay-battery-daemon.service
