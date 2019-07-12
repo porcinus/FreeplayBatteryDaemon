80b8f4208127cacaa1ed17f2067e663a63174607
porcinus <37425709+porcinus@users.noreply.github.com>
2019-07-12 23:56:15 +0200

    Add files via upload
    0.1d update

--- nns-freeplay-battery-daemon.cpp
+++ nns-freeplay-battery-daemon.cpp
@@ -1,9 +1,9 @@
 /*
 NNS @ 2018
 nns-freeplay-battery-daemon
-Battery monitoring daemon Compatible with MCP3021A, LC709203F
+Battery monitoring daemon Compatible with MCP3021A, LC709203F, MAX17048
 */
-const char programversion[]="0.1c"; //program version
+const char programversion[]="0.1d"; //program version
 
 #include <fcntl.h>
 #include <stdio.h>
@@ -37,11 +37,16 @@ int i2c_retry=0;								//reading retry if failure
 char vbat_output_path[PATH_MAX];					//path where output final data
 char vbat_filename[PATH_MAX];							//battery output filename
 char vbat_stats_filename[PATH_MAX];				//battery stats output filename
+char vbat_alarm_filename[PATH_MAX];				//battery alarm output filename
 bool vbat_logging=true;										//enable battery stats output
 float vbat_offset=0.;											//in volt, voltage error offset
 float vbat_value=.0;											//battery voltage, used as backup if read fail
 int vbat_percent_value=0;									//battery percentage
-float temperature_k=0.;										//battery temperature in kelvin
+int vbat_warn_lsoc=4;											//warning value for low SOC
+float vbat_warn_lv=3.1;										//warning value for low voltage
+
+float temperature_k=-1;										//battery temperature in kelvin
+int vbat_alarm=0;													//IC alarm state
 int rsoc_extend[5]={100,0,100,0};					//rsoc extend limits
 char rsoc_extend_raw[1024];								//rsoc extend limits char array
 int rsoc_extend_count=-1;									//rsoc extend limits count
@@ -54,7 +59,12 @@ int adc_raw_value=0;											//adc step value
 
 bool LC709203F_detected=false;		//chip detected bool
 bool LC709203F_init=false;				//used to init registers once
+bool MAX17048_detected=false;		//chip detected bool
+bool MAX17048_init=false;				//used to init registers once
 bool MCP3021A_detected=false;			//chip detected bool
+
+
+
 
 FILE *temp_filehandle;			//file handle to get cpu temp/usage
 int uptime_value=0;					//uptime value
@@ -153,11 +163,10 @@ int LC709203F_read_reg(int addr,char reg){
 	if(debug==2){
 		debug_print("LC709203F_read_reg\n");
 		debug_print("\tregister : 0x%02x\n",reg);
-		debug_print("\ti2c_smbus_read_i2c_block_data returned 0x%04X 0x%02x 0x%02x 0x%02x\n", i2c_read, i2c_buffer[0],i2c_buffer[1],i2c_buffer[2]);
 	}
 
 	if(i2c_read<0){ //oups
-		debug_print("Failed to read register : %02x\n",reg);
+		debug_print("Failed to read register, returned 0x%04X\n",i2c_read);
 	}else{ //success
 		read_crc_array[0] = 0x16; //i2c adress
 		read_crc_array[1] = reg; //register
@@ -188,6 +197,58 @@ int LC709203F_read_reg(int addr,char reg){
 
 
 
+int write_reg16(char reg, int data, bool reverse){
+	i2c_buffer[0]=reg;
+
+	if(reverse){ //split int, reverse order 
+		i2c_buffer[1]=data&0x0FF; //low byte
+		i2c_buffer[2]=(data>>8)&0x0FF; //high byte
+	}else{ //split int
+		i2c_buffer[1]=(data>>8)&0x0FF; //low byte
+		i2c_buffer[2]=data&0x0FF; //high byte
+	}
+
+	if(debug==2){
+		debug_print("write_reg16\n");
+		debug_print("\tregister : 0x%02x\n",reg);
+		debug_print("\tdata : 0x%02x 0x%02x\n",i2c_buffer[1],i2c_buffer[2]);
+	}
+
+	return write(i2c_handle,i2c_buffer,3); //write
+}
+
+
+
+int read_reg16(char reg, bool reverse){
+	unsigned long i2c_read; //i2c read return code
+	int value; //read value
+	
+	i2c_read = i2c_smbus_read_i2c_block_data(i2c_handle, reg, 2, (unsigned char *)i2c_buffer); //write register and read it
+	
+	if(debug==2){
+		debug_print("read_reg16\n");
+		debug_print("\tregister : 0x%02x\n",reg);
+	}
+
+	if(i2c_read<0){ //oups
+		debug_print("Failed to read register, returned 0x%04X\n",i2c_read);
+	}else{ //success
+		if(reverse){value=(i2c_buffer[1]<<8) | (i2c_buffer[0]);} //merge into int, reverse order 
+		else{value=(i2c_buffer[0]<<8) | (i2c_buffer[1]);} //merge into int
+		
+		if(debug==2){
+			debug_print("\tread buffer : 0x%02x 0x%02x\n",i2c_buffer[0],i2c_buffer[1]);
+			debug_print("\tread value : %d\n",value);
+		}
+
+		return value;
+	}
+
+	return -1;
+}
+
+
+
 void show_usage(void){
 	printf(
 "Version: %s\n"
@@ -201,7 +262,10 @@ void show_usage(void){
 "\t-outputpath, Path where data will be saved [Default: /dev/shm]\n"
 "\t-vbatfilename, File that will contain current battery voltage and RSOC, format 'vbat;percent' [Default: vbat.log]\n"
 "\t-vbatstatsfilename, File that will contain battery data from start of the system, format 'uptime;vbat;percent' [Default: vbat-start.log]\n"
+"\t-vbatalarmfilename, File that will contain alarm warning from fuel gauge IC, can contain : LV (low voltage), HV (over voltage), LSOC (low battery), NONE (fine) [Default: vbat-alarm.log]\n"
 "\t-vbatlogging, Set to 0 to disable battery stats data [Default: 1]\n"
+"\t-vbatlowvoltage, Set low voltage warning, in volt [Default: 3.1]\n"
+"\t-vbatlowsoc, Set low SOC warning, in percent [Default: 4]\n"
 "\t-vbatoffset, Battery value offset, in volt, can be positive or negative [Default: 0.00]\n"
 "\t-rsocextend, Extend RSOC value [Default: 100,0,100,0] [Example : FROM_MAX,FROM_MIN,TO_MAX,TO_MIN]\n"
 "\t-adcvref, VRef of the ADC chip in volt [Default: 4.5]\n"
@@ -218,6 +282,8 @@ int main(int argc, char *argv[]){ //main
 	strcpy(vbat_output_path,"/dev/shm/"); //init
 	strcpy(vbat_filename,"vbat.log"); //init
 	strcpy(vbat_stats_filename,"vbat-start.log"); //init
+	strcpy(vbat_alarm_filename,"vbat-alarm.log"); //init
+	
 	
 	for(int i=1;i<argc;++i){ //argument to variable
 		if(strcmp(argv[i],"-help")==0){show_usage();return 1;
@@ -230,8 +296,11 @@ int main(int argc, char *argv[]){ //main
 		
 		}else if(strcmp(argv[i],"-outputpath")==0){strcpy(vbat_output_path,argv[i+1]);if(access(vbat_output_path,W_OK)!=0){debug_print("Failed, %s not writable, Exiting\n",vbat_output_path);return 1;}
 		}else if(strcmp(argv[i],"-vbatfilename")==0){strcpy(vbat_filename,argv[i+1]);
+		}else if(strcmp(argv[i],"-vbatalarmfilename")==0){strcpy(vbat_alarm_filename,argv[i+1]);
 		}else if(strcmp(argv[i],"-vbatstatsfilename")==0){strcpy(vbat_stats_filename,argv[i+1]);
 		}else if(strcmp(argv[i],"-vbatlogging")==0){if(atoi(argv[i+1])<1){vbat_logging=false;}else{vbat_logging=true;}
+		}else if(strcmp(argv[i],"-vbatlowvoltage")==0){vbat_warn_lv=atof(argv[i+1]);
+		}else if(strcmp(argv[i],"-vbatlowsoc")==0){vbat_warn_lsoc=atoi(argv[i+1]);
 		}else if(strcmp(argv[i],"-vbatoffset")==0){vbat_offset=atof(argv[i+1]);if(strstr(argv[i+1],"-")){vbat_offset=vbat_offset*-1;}
 		}else if(strcmp(argv[i],"-rsocextend")==0){strcpy(rsoc_extend_raw,argv[i+1]);rsoc_extend_count=1;
 		
@@ -285,6 +354,7 @@ int main(int argc, char *argv[]){ //main
 	}else{debug_print("Failed, 0x%02x not detected on any bus, Exiting\n",i2c_addr);return(1);} //failed
 		
 	if(i2c_addr==0x0B){LC709203F_detected=true; debug_print("LC709203F detected\n");
+	}else if(i2c_addr==0x36){MAX17048_detected=true; debug_print("MAX17048 detected\n");
 	}else if(i2c_addr>=0x48&&i2c_addr<=0x4F){MCP3021A_detected=true; debug_print("MCP3021A detected\n");
 	}else{debug_print("Failed, I2C address out of range, Exiting\n"); return(1);} //failed
 	
@@ -294,7 +364,7 @@ int main(int argc, char *argv[]){ //main
 		if(debug==2){debug_print("Start update loop\n");}
 		
 		while(i2c_retry<3){
-			adc_raw_value=-1; vbat_value=-1; vbat_percent_value=-1;
+			adc_raw_value=-1; vbat_value=-1; vbat_percent_value=-1; temperature_k=-1;
 			if((i2c_handle=open(i2c_path,O_RDWR))<0){
 				debug_print("Failed to open the I2C bus : %s, retry in %dsec\n",i2c_path,update_duration);
 				i2c_retry=3; //no need to retry since failed to open I2C bus itself
@@ -306,16 +376,15 @@ int main(int argc, char *argv[]){ //main
 						if(ioctl(i2c_handle,I2C_PEC,1)<0){ //enable pec
 							debug_print("Failed to enable PEC for I2C device : %02x\n",i2c_addr);
 						}else{
-							if(!LC709203F_init){ //not done
+							if(!LC709203F_init){ //not init
 								while(get_cpu_load()>2){usleep(500000);} //if CPU load over 2%, sleep 500ms
 								LC709203F_write_reg(i2c_addr,0x15,0x0001); //IC Power Mode
 								LC709203F_write_reg(i2c_addr,0x0B,0x002D); //APA
 								LC709203F_write_reg(i2c_addr,0x12,0x0001); //Select battery profile
 								LC709203F_write_reg(i2c_addr,0x16,0x0000); //Temperature via I2C
 								LC709203F_write_reg(i2c_addr,0x08,0x0BA6); //Temperature at 25°C
-								//LC709203F_write_reg(i2c_addr,0x08,0x0C0A); //Temperature at 35°C
-								//LC709203F_write_reg(i2c_addr,0x16,0x0001); //Temperature via Thermistor
-								//LC709203F_write_reg(i2c_addr,0x06,0x0FA0); //Thermistor B at 4000K
+								LC709203F_write_reg(i2c_addr,0x13,vbat_warn_lsoc); //Alarm Low RSOC
+								LC709203F_write_reg(i2c_addr,0x14,(int)(vbat_warn_lv*1000)); //Alarm Low Cell Voltage
 								for(int i=0;i<i2c_register16_count;i++){LC709203F_write_reg(i2c_addr,i2c_register16_reg[i],i2c_register16_value[i]);} //custom 16bits register
 								//LC709203F_write_reg(i2c_addr,0x07,0xAA55); //Init RSOC
 								//LC709203F_write_reg(i2c_addr,0x04,0xAA55); //Before RSOC
@@ -328,6 +397,22 @@ int main(int argc, char *argv[]){ //main
 							vbat_percent_value=LC709203F_read_reg(i2c_addr,0x0D); //RSOC register
 							temperature_k=(float)LC709203F_read_reg(i2c_addr,0x08)/10; //Cell Temperature in Kelvin
 						}
+					}else if(MAX17048_detected){
+						if(!MAX17048_init){ //not init
+							write_reg16(0x0A,0x0000,false); //HIBRT, disable
+							//write_reg16(0x14,0x9BDC,false); //VALRT, low: 3.1v hi:4.4v
+							write_reg16(0x14,((int)(1+vbat_warn_lv/0.02)<<8)|0xDC,false); //VALRT, low: vbat_warn_lv hi:4.4v
+							write_reg16(0x0C,0x9700|(32-vbat_warn_lsoc),false); //CONFIG, low soc
+							write_reg16(0x1A,0x00FF,false); //STATUS, reset alarm
+							for(int i=0;i<i2c_register16_count;i++){write_reg16(i2c_register16_reg[i],i2c_register16_value[i],false);} //custom 16bits register
+							MAX17048_init=true; //Init done
+							debug_print("MAX17048 initialized\n");
+							sleep(1);
+						}
+						
+						vbat_value=read_reg16(0x02,false)*(double)0.000078125; //Cell Voltage register
+						vbat_percent_value=read_reg16(0x04,false)/256; //SOC register
+						vbat_alarm=read_reg16(0x1A,false); //STATUS register
 					}else if(MCP3021A_detected){
 						if(read(i2c_handle,i2c_buffer,2)!=2){debug_print("Failed to read data from I2C device : %04x, retry in 1sec\n",i2c_addr);
 						}else{ //success
@@ -343,6 +428,18 @@ int main(int argc, char *argv[]){ //main
 					}else{ //success
 						debug_print("Voltage : %.3fv, RSOC : %d%%\n",vbat_value,vbat_percent_value);
 						
+						temp_filehandle=fopen(vbat_alarm_filename,"wb"); //open file handle
+						if(MAX17048_detected&&vbat_alarm!=255){ //alarm MAX17048
+							debug_print("STATUS register : 0x%04X\n",vbat_alarm);
+							if((0x0400&vbat_alarm)==0x0400){debug_print("Warning : Low voltage\n"); fputs("LV\n",temp_filehandle);}
+							if((0x0200&vbat_alarm)==0x0200){debug_print("Warning : Over voltage\n"); fputs("HV\n",temp_filehandle);}
+							if((0x1000&vbat_alarm)==0x1000){debug_print("Warning : SOC %d%\n",vbat_warn_lsoc); fputs("LSOC\n",temp_filehandle);}
+							if(vbat_percent_value>vbat_warn_lsoc){debug_print("Reset STATUS register\n");write_reg16(0x1A,0x00FF,false);} //STATUS, reset alarm
+						}else if(vbat_percent_value<=vbat_warn_lsoc){debug_print("Warning : SOC %d%,%d\n",vbat_warn_lsoc,vbat_percent_value); fputs("LSOC\n",temp_filehandle); //low soc alarm
+						}else if(vbat_value<=vbat_warn_lv){debug_print("Warning : Low voltage\n"); fputs("LV\n",temp_filehandle); //low voltage alarm
+						}else{fputs("NONE\n",temp_filehandle);} //nothing wrong
+						fclose(temp_filehandle); //close handle
+						
 						if(rsoc_extend_count==1){ //custom rsoc limits
 							vbat_percent_value=nns_map_int(vbat_percent_value,rsoc_extend[1],rsoc_extend[0],rsoc_extend[3],rsoc_extend[2]);
 							debug_print("Extended RSOC : %d%%\n",vbat_percent_value);
@@ -356,8 +453,8 @@ int main(int argc, char *argv[]){ //main
 						}
 					}
 					
-					if(temperature_k<0){debug_print("Warning, Cell Temperature probing failed\n");
-					}else{debug_print("Cell Temperature : %.1fK, %.1fC, %.1fF\n",temperature_k,temperature_k-273.15,temperature_k*9/5-459.67);}
+					if(temperature_k<0&&LC709203F_detected){debug_print("Warning, Cell Temperature probing failed\n");
+					}else if(LC709203F_detected){debug_print("Cell Temperature : %.1fK, %.1fC, %.1fF\n",temperature_k,temperature_k-273.15,temperature_k*9/5-459.67);}
 				}
 				close(i2c_handle);
 			}
